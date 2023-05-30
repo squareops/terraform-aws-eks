6ad6b225ed6e639238b881f8761672e24f16a76c
Rohit Singh <107384244+RohitSquareops@users.noreply.github.com>
2023-05-30 12:11:40 +0530

    updated module version, added variables and output (#4)
    Upgraded the module version, Added variable and updated output variable

--- main.tf
+++ main.tf
@@ -1,33 +1,25 @@
-data "aws_subnet_ids" "private_subnet_ids" {
-  vpc_id = var.vpc_id
-  tags = {
-    Subnet-group = "private"
-  }
-}
-
 module "eks" {
   source                    = "terraform-aws-modules/eks/aws"
-  version                   = "18.29.0"
+  version                   = "19.15.0"
   vpc_id                    = var.vpc_id
+  subnet_ids                = var.private_subnet_ids
   enable_irsa               = true
   cluster_name              = format("%s-%s", var.environment, var.name)
-  subnet_ids                = data.aws_subnet_ids.private_subnet_ids.ids
-  cluster_enabled_log_types = var.cluster_log_types
+  create_kms_key            = var.create_kms_key
   cluster_version           = var.cluster_version
+  cluster_enabled_log_types = var.cluster_log_types
   tags = {
     "Name"        = format("%s-%s", var.environment, var.name)
     "Environment" = var.environment
   }
-  cluster_endpoint_private_access        = var.cluster_endpoint_public_access ? false : true
   cluster_endpoint_public_access         = var.cluster_endpoint_public_access
+  cluster_endpoint_private_access        = var.cluster_endpoint_public_access ? false : true
   cluster_endpoint_public_access_cidrs   = var.cluster_endpoint_public_access_cidrs
   cloudwatch_log_group_retention_in_days = var.cluster_log_retention_in_days
-  cluster_encryption_config = [
-    {
-      provider_key_arn = var.kms_key_arn
-      resources        = ["secrets"]
-    }
-  ]
+  cluster_encryption_config = {
+    provider_key_arn = var.kms_key_arn
+    resources        = ["secrets"]
+  }
 }
 
 resource "aws_iam_policy" "kubernetes_pvc_kms_policy" {
@@ -40,8 +32,8 @@ resource "aws_iam_policy" "kubernetes_pvc_kms_policy" {
       {
         "Effect": "Allow",
         "Action": [
-            "kms:CreateGrant",
             "kms:Decrypt",
+            "kms:CreateGrant",
             "kms:GenerateDataKeyWithoutPlaintext"
         ],
         "Resource": "${var.kms_key_arn}"
