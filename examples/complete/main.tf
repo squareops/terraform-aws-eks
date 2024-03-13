2d93cdaa3addaf407cba9b457ce10605bd0ac85f
rachit89 <rachit.maheshwari@squareops.com>
2024-03-13 15:32:45 +0530

    Made changes to the modules for variables

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,36 +1,58 @@
 locals {
-  region      = "us-west-2"
-  environment = "prod"
-  name        = "eks"
+  aws_region                               = "us-east-1"
+  aws_account_id                           = ""
+  kms_deletion_window_in_days              = 7
+  key_rotation_enabled                     = true
+  is_enabled                               = true
+  multi_region                             = true
+  environment                              = "prod"
+  name                                     = "eks"
+  vpc_availability_zones                   = ["us-east-1a", "us-east-1b"]
+  vpc_public_subnet_enabled                = true
+  vpc_private_subnet_enabled               = true
+  database_subnet_enabled                  = false
+  vpc_intra_subnet_enabled                 = false
+  vpc_one_nat_gateway_per_az               = true
+  vpn_server_instance_type                 = "t3a.small"
+  vpc_flow_log_enabled                     = false
+  kms_user                                 = null
+  vpc_cidr                                 = "10.10.0.0/16"
+  vpn_server_enabled                       = false
+  default_addon_enabled                    = false
+  eks_cluster_version                      = "1.27"
+  eks_cluster_log_types                    = []
+  eks_cluster_log_retention_in_days        = 30
+  eks_capacity_type                        = "SPOT"
+  managed_node_group_capacity_type         = "SPOT"
+  eks_cluster_endpoint_public_access       = true
+  eks_cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+  create_aws_auth_configmap                = true
+  node_group_ebs_volume_size               = 50
+  current_identity                         = data.aws_caller_identity.current.arn
   additional_aws_tags = {
     Owner      = "Organization_name"
     Expires    = "Never"
     Department = "Engineering"
   }
-  kms_user              = null
-  vpc_cidr              = "10.10.0.0/16"
-  vpn_server_enabled    = false
-  default_addon_enabled = false
-  current_identity      = data.aws_caller_identity.current.arn
 }
 data "aws_caller_identity" "current" {}
 
 module "kms" {
   source = "terraform-aws-modules/kms/aws"
 
-  deletion_window_in_days = 7
+  deletion_window_in_days = local.kms_deletion_window_in_days
   description             = "Symetric Key to Enable Encryption at rest using KMS services."
-  enable_key_rotation     = true
-  is_enabled              = true
+  enable_key_rotation     = local.key_rotation_enabled
+  is_enabled              = local.is_enabled
   key_usage               = "ENCRYPT_DECRYPT"
-  multi_region            = true
+  multi_region            = local.multi_region
 
   # Policy
   enable_default_policy                  = true
   key_owners                             = [local.current_identity]
-  key_administrators                     = local.kms_user == null ? ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling", "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS", local.current_identity] : local.kms_user
-  key_users                              = local.kms_user == null ? ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling", "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS", local.current_identity] : local.kms_user
-  key_service_users                      = local.kms_user == null ? ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling", "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS", local.current_identity] : local.kms_user
+  key_administrators                     = local.kms_user == null ? ["arn:aws:iam::${local.aws_account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling", "arn:aws:iam::${local.aws_account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS", local.current_identity] : local.kms_user
+  key_users                              = local.kms_user == null ? ["arn:aws:iam::${local.aws_account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling", "arn:aws:iam::${local.aws_account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS", local.current_identity] : local.kms_user
+  key_service_users                      = local.kms_user == null ? ["arn:aws:iam::${local.aws_account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling", "arn:aws:iam::${local.aws_account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS", local.current_identity] : local.kms_user
   key_symmetric_encryption_users         = [local.current_identity]
   key_hmac_users                         = [local.current_identity]
   key_asymmetric_public_encryption_users = [local.current_identity]
@@ -51,7 +73,7 @@ module "kms" {
       principals = [
         {
           type        = "Service"
-          identifiers = ["logs.${local.region}.amazonaws.com"]
+          identifiers = ["logs.${local.aws_region}.amazonaws.com"]
         }
       ]
     }
@@ -82,44 +104,44 @@ module "vpc" {
   environment                                     = local.environment
   name                                            = local.name
   vpc_cidr                                        = local.vpc_cidr
-  availability_zones                              = ["us-west-2a", "us-west-2b"]
-  public_subnet_enabled                           = true
-  private_subnet_enabled                          = true
-  database_subnet_enabled                         = false
-  intra_subnet_enabled                            = false
-  one_nat_gateway_per_az                          = true
+  availability_zones                              = local.vpc_availability_zones
+  public_subnet_enabled                           = local.vpc_public_subnet_enabled
+  private_subnet_enabled                          = local.vpc_private_subnet_enabled
+  database_subnet_enabled                         = local.database_subnet_enabled
+  intra_subnet_enabled                            = local.vpc_intra_subnet_enabled
+  one_nat_gateway_per_az                          = local.vpc_one_nat_gateway_per_az
   vpn_server_enabled                              = local.vpn_server_enabled
-  vpn_server_instance_type                        = "t3a.small"
+  vpn_server_instance_type                        = local.vpn_server_instance_type
   vpn_key_pair_name                               = local.vpn_server_enabled ? module.key_pair_vpn[0].key_pair_name : null
-  flow_log_enabled                                = false
+  flow_log_enabled                                = local.vpc_flow_log_enabled
   flow_log_max_aggregation_interval               = 60
   flow_log_cloudwatch_log_group_retention_in_days = 90
   flow_log_cloudwatch_log_group_kms_key_arn       = module.kms.key_arn
 }
 
 module "eks" {
-  source                               = "squareops/eks/aws"
-  depends_on                           = [module.vpc]
-  name                                 = local.name
-  vpc_id                               = module.vpc.vpc_id
-  subnet_ids                           = [module.vpc.private_subnets[0]]
-  min_size                             = 1
-  max_size                             = 3
-  desired_size                         = 1
-  ebs_volume_size                      = 50
-  capacity_type                        = "ON_DEMAND"
-  instance_types                       = ["t3a.large", "t2.large", "t2.xlarge", "t3.large", "m5.large"]
-  environment                          = local.environment
-  kms_key_arn                          = module.kms.key_arn
-  cluster_version                      = "1.27"
-  cluster_log_types                    = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
-  private_subnet_ids                   = module.vpc.private_subnets
-  cluster_log_retention_in_days        = 30
-  cluster_endpoint_public_access       = true
-  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
-  create_aws_auth_configmap            = true
-  default_addon_enabled                = local.default_addon_enabled
-  eks_nodes_keypair_name               = module.key_pair_eks.key_pair_name
+  source                                   = "squareops/eks/aws"
+  depends_on                               = [module.vpc]
+  name                                     = local.name
+  vpc_id                                   = module.vpc.vpc_id
+  subnet_ids                               = [module.vpc.private_subnets[0]]
+  min_size                                 = 1
+  max_size                                 = 3
+  desired_size                             = 1
+  ebs_volume_size                          = local.node_group_ebs_volume_size
+  capacity_type                            = local.eks_capacity_type
+  instance_types                           = ["t3a.large", "t2.large", "t2.xlarge", "t3.large", "m5.large"]
+  environment                              = local.environment
+  kms_key_arn                              = module.kms.key_arn
+  eks_cluster_version                      = local.eks_cluster_version
+  eks_cluster_log_types                    = local.eks_cluster_log_types
+  vpc_private_subnet_ids                   = module.vpc.private_subnets
+  eks_cluster_log_retention_in_days        = local.eks_cluster_log_retention_in_days
+  eks_cluster_endpoint_public_access       = local.eks_cluster_endpoint_public_access
+  eks_cluster_endpoint_public_access_cidrs = local.eks_cluster_endpoint_public_access_cidrs
+  create_aws_auth_configmap                = local.create_aws_auth_configmap
+  default_addon_enabled                    = local.default_addon_enabled
+  eks_nodes_keypair_name                   = module.key_pair_eks.key_pair_name
   aws_auth_roles = [
     {
       rolearn  = "arn:aws:iam::222222222222:role/service-role"
@@ -134,7 +156,7 @@ module "eks" {
       groups   = ["system:masters"]
     },
   ]
-  additional_rules = {
+  eks_cluster_security_group_additional_rules = {
     ingress_port_mgmt_tcp = {
       description = "mgmt vpc cidr"
       protocol    = "tcp"
@@ -156,11 +178,11 @@ module "managed_node_group_production" {
   subnet_ids             = [module.vpc.private_subnets[0]]
   environment            = local.environment
   kms_key_arn            = module.kms.key_arn
-  capacity_type          = "ON_DEMAND"
+  capacity_type          = local.managed_node_group_capacity_type
   ebs_volume_size        = 50
   instance_types         = ["t3a.large", "t2.large", "t2.xlarge", "t3.large", "m5.large"]
   kms_policy_arn         = module.eks.kms_policy_arn
-  eks_cluster_name       = module.eks.cluster_name
+  eks_cluster_name       = module.eks.eks_cluster_name
   default_addon_enabled  = local.default_addon_enabled
   worker_iam_role_name   = module.eks.worker_iam_role_name
   worker_iam_role_arn    = module.eks.worker_iam_role_arn
@@ -177,7 +199,7 @@ module "farget_profle" {
   profile_name = "app"
   subnet_ids   = [module.vpc.private_subnets[0]]
   environment  = local.environment
-  cluster_name = module.eks.cluster_name
+  cluster_name = module.eks.eks_cluster_name
   namespace    = ""
   labels = {
     "App-Services" = "fargate"
--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,31 +1,31 @@
-output "region" {
+output "aws_region" {
   description = "AWS region in which the EKS cluster is created."
-  value       = local.region
+  value       = local.aws_region
 }
 
-output "cluster_name" {
+output "eks_cluster_name" {
   description = "Name of the Kubernetes cluster."
-  value       = module.eks.cluster_name
+  value       = module.eks.eks_cluster_name
 }
 
-output "cluster_endpoint" {
+output "eks_cluster_endpoint" {
   description = "Endpoint URL for the EKS control plane."
-  value       = module.eks.cluster_endpoint
+  value       = module.eks.eks_cluster_endpoint
 }
 
 output "cluster_security_group_id" {
   description = "Security group IDs that are attached to the control plane of the EKS cluster."
-  value       = module.eks.cluster_security_group_id
+  value       = module.eks.eks_cluster_security_group_id
 }
 
 output "cluster_arn" {
   description = "ARN of the EKS Cluster."
-  value       = module.eks.cluster_arn
+  value       = module.eks.eks_cluster_arn
 }
 
 output "cluster_oidc_issuer_url" {
   description = "URL of the OpenID Connect identity provider on the EKS cluster."
-  value       = module.eks.cluster_oidc_issuer_url
+  value       = module.eks.eks_cluster_oidc_issuer_url
 }
 
 output "worker_iam_role_arn" {
