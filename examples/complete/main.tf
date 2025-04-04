79ebcd232023c54803f7174f8614dc82980b43d1
ankush-sqops <ankush.upadhyay@squareops.com>
2025-04-04 10:45:27 +0530

    Merge pull request #77 from squareops/release-5.4.3
    Release 5.4.3

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -139,7 +139,7 @@ module "vpc" {
 
 module "eks" {
   source               = "squareops/eks/aws"
-  version              = "5.4.2"
+  version              = "5.4.3"
   access_entry_enabled = true
   access_entries = {
     "example" = {
@@ -158,7 +158,6 @@ module "eks" {
   }
   enable_cluster_creator_admin_permissions = true
   authentication_mode                      = "API_AND_CONFIG_MAP"
-  depends_on                               = [module.vpc]
   name                                     = local.name
   vpc_id                                   = module.vpc.vpc_id
   environment                              = local.environment
@@ -188,8 +187,8 @@ module "eks" {
 
 module "managed_node_group_addons" {
   source                      = "squareops/eks/aws//modules/managed-nodegroup"
-  version                     = "5.4.2"
-  depends_on                  = [module.vpc, module.eks]
+  version                     = "5.4.3"
+  depends_on                  = [module.eks]
   managed_ng_name             = "Infra"
   managed_ng_min_size         = 2
   managed_ng_max_size         = 5
